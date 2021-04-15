40bd6dce473b4dbf4293f3f71111835adc12d14b
MatheusFuzi <80868115+MatheusFuzi@users.noreply.github.com>
2021-04-15 00:23:37 -0300

    Add files via upload

---
+++ aareb1/src/main/java/br/com/unipar/Login/Login.java
@@ -0,0 +1,67 @@
+package br.com.unipar.Login;
+
+import java.util.Scanner;
+
+public class Login {
+
+    String name, user, password, confirmpassword;
+    Integer id;
+
+    void reading() {
+
+        this.id = 10;
+        System.out.printf("Digite seu nome:");
+        Scanner read = new Scanner(System.in);
+        this.name = read.nextLine();
+        System.out.printf("Digite sua senha:");
+        Scanner read1 = new Scanner(System.in);
+        this.password = read1.nextLine();
+        System.out.printf("Digite seu usuario:");
+        Scanner read2 = new Scanner(System.in);
+        this.user = read2.nextLine();
+        System.out.printf("Confirme sua senha:");
+        Scanner read3 = new Scanner(System.in);
+        this.confirmpassword = read3.nextLine();
+
+        confirm();
+
+    }
+
+    void confirm() {
+
+        if (this.confirmpassword.equals(this.password)) {
+            verify(); return;
+        }
+        
+        if (verify()) {
+            System.err.println("Login nao realizado");
+        }
+    }
+
+    boolean verify() {
+        if (this.name.length() < 10) {
+            System.err.println("Nome menor que 10 letras");
+            return false;
+        }
+        if (this.password.length() < 8) {
+            System.err.println("Senha menor que 8 digitos: fraca");
+            return false;
+        } 
+        if (this.password.equals(this.confirmpassword)) {
+            System.out.println("login realizado com sucesso");
+            return true;
+        }       
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Login user1 = new Login();
+        Login user2 = new Login();
+        Login user3 = new Login();
+        user1.reading();
+        user2.reading();
+        user3.reading();
+        return;
+
+    }
+}
