5565d3e323d637841af45244f8d65e9cdd2c57e9
MatheusFuzi <80868115+MatheusFuzi@users.noreply.github.com>
2021-04-17 17:04:02 -0300

    Update Login.java

--- aareb1/src/main/java/br/com/unipar/Login/Login.java
+++ aareb1/src/main/java/br/com/unipar/Login/Login.java
@@ -10,6 +10,8 @@ public class Login {
     void reading() {
 
         this.id = 10;
+        System.out.println("\n");
+        System.out.println("Cadastre o seu Login");
         System.out.printf("Digite seu nome:");
         Scanner read = new Scanner(System.in);
         this.name = read.nextLine();
@@ -22,38 +24,27 @@ public class Login {
         System.out.printf("Confirme sua senha:");
         Scanner read3 = new Scanner(System.in);
         this.confirmpassword = read3.nextLine();
-
         confirm();
-
+        System.out.printf("\n");
     }
 
     void confirm() {
 
-        if (this.confirmpassword.equals(this.password)) {
-            verify(); return;
-        }
-        
-        if (verify()) {
-            System.err.println("Login nao realizado");
-        }
+        if (this.confirmpassword.equals(this.password)) { verify(); }else  
+        System.err.println("Login nao cadastrado, senhas não conferem"); 
     }
 
     boolean verify() {
-        if (this.name.length() < 10) {
-            System.err.println("Nome menor que 10 letras");
-            return false;
-        }
-        if (this.password.length() < 8) {
-            System.err.println("Senha menor que 8 digitos: fraca");
-            return false;
-        } 
+        if (this.name.length() < 10) { System.err.println("Nome menor que 10 letras");
+            return false;}
+        if (this.password.length() < 8) { System.err.println("Senha menor que 8 digitos: fraca");
+            return false; } 
         if (this.password.equals(this.confirmpassword)) {
-            System.out.println("login realizado com sucesso");
+            System.out.println("login cadastrado com sucesso");
             System.out.println("Usuario: " + this.user);
             System.out.println("Nome: "+this.name);
             System.out.println("Senha: "+ this.password);
-            return true;
-        }       
+            return true;}       
         return false;
     }
 
@@ -64,7 +55,5 @@ public class Login {
         user1.reading();
         user2.reading();
         user3.reading();
-        return;
-
     }
 }
